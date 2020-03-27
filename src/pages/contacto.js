5c36a2abc50767d60be8f53056a9c880103c1d4e
eljuliopo <eljuliopo@gmail.com>
2020-03-26 22:03:32 -0300

    Update contacto.js

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -38,7 +38,7 @@ export default ({ data }) => (
         </Form.Field>
         <Form.TextArea required>
           <label>Mensaje</label>
-          <textarea name='message' placeholder='Déjanos tu mensaje' />
+          <input type='text' name='message' placeholder='Déjanos tu mensaje' />
         </Form.TextArea>
       </Form.Group>
       <Form.Field required>
